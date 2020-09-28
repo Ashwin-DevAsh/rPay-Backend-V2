e565e191b8f97d10ffa60ada6edc81f209a04003
Ashwin-DevAsh <2022ashwin@gmail.com>
2020-09-28 18:39:21 +0530

    first commit

--- RecWalletBackend/src/main/kotlin/com/DevAsh/RecWalletBackend/Security/SecurityConfig.kt
+++ RecWalletBackend/src/main/kotlin/com/DevAsh/RecWalletBackend/Security/SecurityConfig.kt
@@ -5,6 +5,8 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter
+
 @Configuration
 class SecurityConfig : WebSecurityConfigurerAdapter() {
 
@@ -15,8 +17,7 @@ class SecurityConfig : WebSecurityConfigurerAdapter() {
         http!!
                 .httpBasic().disable()
                 .csrf().disable()
-                // .antMatcher("/api/v1/account/protected/*")
-                // .addFilterBefore(jwtFilter, BasicAuthenticationFilter::class.java)
+                .antMatcher("/api/v1/account/protected/**")
+                .addFilterBefore(jwtFilter, BasicAuthenticationFilter::class.java)
     }
-}
 }
--- RecWalletBackend/src/main/kotlin/com/DevAsh/RecWalletBackend/Security/filters/JwtFilter.kt
+++ RecWalletBackend/src/main/kotlin/com/DevAsh/RecWalletBackend/Security/filters/JwtFilter.kt
@@ -2,17 +2,17 @@ package com.DevAsh.RecWalletBackend.Security.filters
 
 import com.DevAsh.RecWalletBackend.Dao.AccountDao
 import io.jsonwebtoken.Jwts
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.beans.factory.annotation.Qualifier
-import org.springframework.beans.factory.annotation.Value
-import org.springframework.stereotype.Component
 import javax.servlet.FilterChain
 import javax.servlet.http.HttpFilter
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Component
 
 @Component
-class JwtFilter(): HttpFilter() {
+class JwtFilter() : HttpFilter() {
 
     @Value("\${jwt.secret}")
     lateinit var secretKey: String
@@ -22,32 +22,30 @@ class JwtFilter(): HttpFilter() {
     lateinit var accountDao: AccountDao
 
     override fun doFilter(
-            request: HttpServletRequest?,
-            response: HttpServletResponse?,
-            chain: FilterChain?
+        request: HttpServletRequest?,
+        response: HttpServletResponse?,
+        chain: FilterChain?
     ) {
         val header = request?.getHeader("token")
-        if(header==null){
+        println("filtering ..." + header)
+        if (header == null) {
             response?.status = HttpServletResponse.SC_UNAUTHORIZED
-        }else if(!checkJwt(header)){
+        } else if (!checkJwt(header)) {
             response?.status = HttpServletResponse.SC_UNAUTHORIZED
-        }else{
+        } else {
             super.doFilter(request, response, chain)
         }
     }
 
-
-    fun checkJwt(jwtString: String):Boolean{
-        try{
-            val id:String? = Jwts.parser().setSigningKey(secretKey.toByteArray()).parseClaimsJws(jwtString).body["id"] as String?
-            if(accountDao.isPayAccountExist(id!!)!=null){
+    fun checkJwt(jwtString: String): Boolean {
+        try {
+            val id: String? = Jwts.parser().setSigningKey(secretKey.toByteArray()).parseClaimsJws(jwtString).body["id"] as String?
+            if (accountDao.isPayAccountExist(id!!) != null) {
                 return true
             }
-            return  false
-        }catch (e:Throwable){
+            return false
+        } catch (e: Throwable) {
             return false
         }
     }
-
-
 }
