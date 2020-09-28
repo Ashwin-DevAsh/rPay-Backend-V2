ca39f369a2b1658c1dfccd71a153835c398335bd
Ashwin-DevAsh <2022ashwin@gmail.com>
2020-09-28 18:33:02 +0530

    first commit

--- RecWalletBackend/src/main/kotlin/com/DevAsh/RecWalletBackend/Security/SecurityConfig.kt
+++ RecWalletBackend/src/main/kotlin/com/DevAsh/RecWalletBackend/Security/SecurityConfig.kt
@@ -5,8 +5,6 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
-import org.springframework.security.web.authentication.www.BasicAuthenticationFilter
-
 @Configuration
 class SecurityConfig : WebSecurityConfigurerAdapter() {
 
@@ -17,7 +15,8 @@ class SecurityConfig : WebSecurityConfigurerAdapter() {
         http!!
                 .httpBasic().disable()
                 .csrf().disable()
-                .antMatcher("/api/v1/account/protected/*")
-                .addFilterBefore(jwtFilter, BasicAuthenticationFilter::class.java)
+                // .antMatcher("/api/v1/account/protected/*")
+                // .addFilterBefore(jwtFilter, BasicAuthenticationFilter::class.java)
     }
+}
 }
